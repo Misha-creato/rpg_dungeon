76e067d92b02c7a33c68f6c62161ab1f25acbaae
Iri O <oparina.iri@gmail.com>
2024-03-16 17:35:45 +0500

    Constants file

---
+++ constants.py
@@ -0,0 +1,30 @@
+MIN_MONSTERS_QUANTITY = 2
+MAX_MONSTERS_QUANTITY = 5
+
+MONSTERS_INFO: list[dict] = [
+    {
+        'name': 'Rat',
+        'hp': 10,
+        'attack': 3,
+    },
+    {
+        'name': 'Goblin',
+        'hp': 18,
+        'attack': 7,
+    },
+    {
+        'name': 'Main Goblin',
+        'hp': 25,
+        'attack': 10,
+    },
+    {
+        'name': 'Young Dragon',
+        'hp': 37,
+        'attack': 8,
+    },
+    {
+        'name': 'Dragon',
+        'hp': 50,
+        'attack': 15,
+    },
+]
