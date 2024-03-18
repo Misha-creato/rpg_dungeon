699a1a5a649062b9e7d9be7ac7239ef77e55906a
Iri O <oparina.iri@gmail.com>
2024-03-18 05:14:42 +0500

    get_character_info in Interface class

--- interface.py
+++ interface.py
@@ -1,12 +1,13 @@
+import time
 from characters import Character
-from monster import Monster
+from monsters import Monster
 
 
 class Interface:
 
     end_messages: dict = {
         'dead': 'One of your characters is dead. You cannot continue game.',
-        'exit': 'You choose to stop game.',
+        'exit': 'You chose to stop game.',
     }
 
     choice_messages: dict = {
@@ -18,7 +19,7 @@ class Interface:
     character_move_options: dict = {
         '1': 'Use simple attack',
         '2': 'Use ability',
-        'exit': 'Exit'
+        'q': 'Exit'
     }
 
     def get_player_choice(self, message_key: str, options: dict = None, can_use_ability: bool = False):
@@ -29,26 +30,39 @@ class Interface:
                 options.pop('2')
         else:
             options = options.copy()
-            options.update({'exit': 'Exit game'})
+            options.update({'q': 'Exit game'})
         choice_message = self.choice_messages[message_key]
         return self.ask_player_choice(choice_message=choice_message, options=options)
 
     def ask_player_choice(self, choice_message: str, options: dict):
         while True:
-            print(choice_message)
+            print('\n', choice_message)
             for key, value in options.items():
+                if type(value) != str:
+                    value = self.get_character_info(character=value)
+                time.sleep(0.5)
                 print(f'{key}. {value}')
-            player_choice = input('Input choice: ')
+            player_choice = input('\n Input choice: ')
             if player_choice in options.keys():
                 return player_choice
             print('Invalid choice')
 
-    def print_monster_dead(self, name: str, mana_points: int):
-        print(f'Monster {name} died!')
+    def get_character_info(self, character: Character | Monster):
+        if isinstance(character, Monster):
+            return f'{character.__class__.__name__} [HP: {character.hp}]'
+        return (f'{character.__class__.__name__} '
+         f'[HP: {character.hp} ATTACK: {character.attack} MANA: {character.mana} '
+         f'ABILITY: {character.ability_info}]')
+
+    def print_monster_dead(self, monster: Monster, mana_points: int):
+        time.sleep(0.5)
+        print(f'Monster {monster.__class__.__name__} died!')
+        time.sleep(0.5)
         print(f'All characters increase their mana for {mana_points} points')
 
-    def print_character_dead(self, name: str):
-        print(f'Character {name} died...')
+    def print_character_dead(self, character: Character):
+        time.sleep(1)
+        print(f'Character {character.__class__.__name__} died...')
 
     def print_start_game(self, characters: list):
         print('Hello!')
@@ -56,23 +70,26 @@ class Interface:
         print(f'Your characters: {characters_name}')
 
     def print_main_loop_info(self, monsters_quantity: int):
+        time.sleep(0.5)
         print(f'You meet {monsters_quantity} monsters on your way!')
 
     def print_characters_turn(self):
+        time.sleep(0.5)
         print('Characters are ready for fight!!!')
 
     def print_monsters_turn(self):
+        time.sleep(0.5)
         print('Beware! Monsters are going to attack!!!')
 
-    def print_monster_move(self, monster: Monster):
-        print(f'Monster {monster.name} attacks!')
-
-    def print_simple_attack(self, attacking: Character | Monster, victim: Character | Monster, damage: int):
-        print(f'{attacking.__class__.__name__} attacks {victim.__class__.__name__} and deals {damage} damage points')
+    def print_use_simple_attack(self, attacking: Character | Monster, victim: Character | Monster, damage: int):
+        time.sleep(1)
+        print(f'\n{attacking.__class__.__name__} attacks {victim.__class__.__name__} and deals {damage} damage points')
 
     def print_use_ability(self, message: str):
+        time.sleep(1)
         print(f'{message}')
 
     def print_end_game(self, message_key: str):
+        time.sleep(0.5)
         message = self.end_messages[message_key]
         print(message)
