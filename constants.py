5d618d1cd03756320ac66cb07cc114dbca898992
Iri O <oparina.iri@gmail.com>
2024-03-18 05:05:54 +0500

    Added CHARACTERS_CLASSES, MONSTERS_CLASSES

--- constants.py
+++ constants.py
@@ -1,30 +1,17 @@
+from characters import (
+    Wizard,
+    Paladin,
+    Priest,
+)
+from monsters import (
+    Rat,
+    Goblin,
+    MainGoblin,
+    YoungDragon,
+    Dragon,
+)
+
 MIN_MONSTERS_QUANTITY = 2
 MAX_MONSTERS_QUANTITY = 5
-
-MONSTERS_INFO: list[dict] = [
-    {
-        'name': 'Rat',
-        'hp': 10,
-        'attack': 3,
-    },
-    {
-        'name': 'Goblin',
-        'hp': 18,
-        'attack': 7,
-    },
-    {
-        'name': 'Main Goblin',
-        'hp': 25,
-        'attack': 10,
-    },
-    {
-        'name': 'Young Dragon',
-        'hp': 37,
-        'attack': 8,
-    },
-    {
-        'name': 'Dragon',
-        'hp': 50,
-        'attack': 15,
-    },
-]
+CHARACTERS_CLASSES = [Wizard, Paladin, Priest]
+MONSTERS_CLASSES = [Rat, Goblin, MainGoblin, YoungDragon, Dragon]
